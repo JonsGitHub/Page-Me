c078cfc183ac7a17c4d8a9f4edc3e13664f8cd32
cloud <cloud@clouds-MBP.local.tld>
2015-07-22 12:27:36 -0700

    Message

--- Page Me/main.m
+++ Page Me/main.m
@@ -11,6 +11,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        NSLog(@"Hello, World!");
     }
+    return 0;
 }
